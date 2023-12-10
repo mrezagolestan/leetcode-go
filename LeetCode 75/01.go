87d3dd357616e97786815b05f105ec3d1cf408dd
mreza_golestan <glpat-HNp5GD7cruP6JVwFC-2W>
2023-12-10 09:48:17 +0330

    change readme

--- LeetCode 75/01.go
+++ LeetCode 75/01.go
@@ -1,9 +1,11 @@
 /*
- * https://leetcode.com/problems/merge-strings-alternately
- * You are given two strings word1 and word2.
- * Merge the strings by adding letters in alternating order, starting with word1. 
- * If a string is longer than the other, append the additional letters onto the end of the merged string.
- */
+* --- 1768. Merge Strings Alternately ---
+* https://leetcode.com/problems/merge-strings-alternately
+* You are given two strings word1 and word2.
+* Merge the strings by adding letters in alternating order, starting with word1. 
+* If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+*/
 package main
 import ("fmt")
 
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# leetcode-go
+# LeetCode Practices
+here i'll practice leet code problems and put it there
