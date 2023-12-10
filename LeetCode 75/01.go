121e307b03ed3a93cf94ab62fac00c873cc83900
mreza_golestan <glpat-HNp5GD7cruP6JVwFC-2W>
2023-12-10 09:46:51 +0330

    01 - merge string alternatively

---
+++ LeetCode 75/01.go
@@ -0,0 +1,40 @@
+/*
+ * https://leetcode.com/problems/merge-strings-alternately
+ * You are given two strings word1 and word2.
+ * Merge the strings by adding letters in alternating order, starting with word1. 
+ * If a string is longer than the other, append the additional letters onto the end of the merged string.
+ */
+package main
+import ("fmt")
+
+func main(){
+    fmt.Println(mergeAlternately("abc","pqr"));
+    fmt.Println(mergeAlternately("ab","pqrs"));
+    fmt.Println(mergeAlternately("abcd","pq"));
+    
+}
+func mergeAlternately(word1 string, word2 string) string {
+    var final string
+    lenWord1 := len(word1)
+    lenWord2 := len(word2)
+    iteration := max(lenWord1, lenWord2)
+
+
+    for i := 0; i < iteration; i++ {
+        if i < lenWord1 {
+            final += string(word1[i])
+        }
+        if i < lenWord2 {
+            final += string(word2[i])
+        }
+    }
+    return final
+}
+
+func max(number1 int, number2 int) int {
+    if number1 > number2 {
+        return number1;
+    }else{
+        return number2;
+    }
+}
