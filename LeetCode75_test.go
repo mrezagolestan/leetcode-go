b2b654e3a5a6500cd82da2ba14eb8a9a748b37d1
mreza_golestan <glpat-HNp5GD7cruP6JVwFC-2W>
2023-12-26 10:19:35 +0330

    basic

---
+++ LeetCode75_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"testing"
+)
+
+func testGcdOfStrings(t *testing.T) {
+	//fmt.Println(LeetCode75.GcdOfStrings("OBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNO","OBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNO"))
+	t.Errorf("Get")
+}
