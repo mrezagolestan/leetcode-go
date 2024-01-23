fd743129f24f2ddb826268a5284ea138ed28bbe2
mreza_golestan <>
2024-01-23 16:19:06 +0330

    files: 1

---
+++ files/blogposts/blogposts_test.go
@@ -0,0 +1,33 @@
+package blogposts_test
+
+import (
+	"errors"
+	"io/fs"
+	"leetcode-go/files/blogposts"
+	"testing"
+	"testing/fstest"
+)
+
+func TestNewBlogPosts(t *testing.T) {
+	fs := fstest.MapFS{
+		"hello world.md":  {Data: []byte("hi")},
+		"hello-world2.md": {Data: []byte("hola")},
+	}
+
+	posts, err := blogposts.NewPostsFromFS(fs)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(posts) != len(fs) {
+		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+}
+
+type StubFailingFS struct {
+}
+
+func (s StubFailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("oh no, i always fail")
+}
